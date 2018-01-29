7ba5b6a39f4849d28e0c396311cd6f921bafb5b0
Nicholas <dojoman19@gmail.com>
2018-01-28 23:44:31 -0500

    Started reorganizing and understanding more of Love2D

--- game/main.lua
+++ game/main.lua
@@ -1,11 +1,12 @@
 class = require 'lib.middleclass'
 util = require 'lib.util'
+require 'lib.physics'
+
 require 'classes.entity'
 require 'classes.trabbit'
 require 'classes.entities.chest'
 require 'classes.entities.door'
 require 'classes.entities.heart'
-require 'lib.physics'
 
 function love.load()
 	player = trabbit:new(0, 0)
@@ -20,22 +21,26 @@ function love.load()
 end 
 
 function love.draw()
-	local collision
-	local x, y, width, length 
+	love.graphics.clear()
 	love.graphics.setScreen('top')
 	love.graphics.setColor(0, 0, 200)
 	love.graphics.rectangle("fill", 0, 0, 400, 240)
 	love.graphics.setColor(255, 255, 255)
 	objects["door1"]:draw()
 	objects["door2"]:draw()	
-	x, y = objects["door1"]:getPosition()
-	width, length = objects["door1"]:getDimensions()
-	collisions = checkrectangle(x, y, width, length, {"door2"}, self)
-	if #collisions == 0 then 
-		love.graphics.print("Success! No collision", 100, 200, 100)  
+	if #collision == 0 then 
+		love.graphics.print("No collision", 100, 200, 100)  
+	else
+		love.graphics.print("Collision", 100, 200, 100)
 	end
 	love.graphics.setColor(255, 255, 255)
 	love.graphics.print(player:getName(), 100, 100, 100)
 	love.graphics.setScreen('bottom')
 	love.graphics.print(tostring(chest_test:isInvincible()), 100, 100)
+end
+
+function love.update(dt)
+	x, y = objects["door1"]:getPosition()
+	width, length = objects["door1"]:getDimensions()
+	collision = checkrectangle(x, y, width, length, {"door2"}, self)
 end
