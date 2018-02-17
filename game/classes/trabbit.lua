f741fa4351d239d773ab9fb57c2c5f491fd93d45
Nicholas Jones <dojoman19@gmail.com>
2018-02-17 16:59:11 -0500

    In process of trying to get the trabbit walking and breathing animations working

--- game/classes/trabbit.lua
+++ game/classes/trabbit.lua
@@ -12,7 +12,18 @@ function trabbit:init(x, y)
 	self.length = 30
 
 	self.breathing = {}
+	self.walking = {}
 	self.prevTime = start
+
+	self.stateEnum = {
+		breathing = 0,
+		walking = 1, 
+		jumping = 2	
+	}
+
+	self.currState = self.stateEnum["breathing"]
+	self.prevInput = {}
+	self.currInput = {}
 end
 
 --[[
@@ -20,13 +31,23 @@ end
 --]]
 function trabbit:draw()
 	--if not flipped, draw the regular image
-	if self.isFlipped == false then 
-		love.graphics.draw(self.image, self.breathing[self.activeFrame], 
-			math.floor(self.x), math.floor(self.y))
+	if self.isFlipped == false then
+		if self.currState == self.stateEnum["breathing"] then 
+			love.graphics.draw(self.image, self.breathing[self.activeFrame], 
+				math.floor(self.x), math.floor(self.y))
+		elseif self.currState == self.stateEnum["walking"] then 
+			love.graphics.draw(self.image, self.walking[self.activeFrame], 
+				math.floor(self.x), math.floor(self.y))
+		end
 	--otherwise, draw the flipped image 
-	else 
-		love.graphics.draw(self.image, self.breathing[self.activeFrame], 
-			math.floor(self.x), math.floor(self.y), rotation, -1, 1)
+	else
+		if self.currState == self.stateEnum["breathing"] then 
+			love.graphics.draw(self.image, self.breathing[self.activeFrame], 
+				math.floor(self.x), math.floor(self.y), rotation, -1, 1)
+		elseif self.currState == self.stateEnum["walking"] then 
+			love.graphics.draw(self.image, self.walking[4], 
+				math.floor(self.x), math.floor(self.y), rotation, -1, 1)
+		end 
 	end
 	--print the active frame for debugging 
 	love.graphics.print(tostring(self.activeFrame), 100, 50)
@@ -42,6 +63,11 @@ function trabbit:setSprites()
 	self.breathing[3] = self.breathing[1]
 	self.breathing[4] = love.graphics.newQuad(109, 2, 35, 32)
 
+	self.walking[1] = love.graphics.newQuad(0, 33, 35, 32)
+	self.walking[2] = love.graphics.newQuad(35, 33, 37, 32)
+	self.walking[3] = self.walking[1]
+	self.walking[4] = love.graphics.newQuad(109, 33, 34, 32)
+
 	self.activeFrame = 1
 end
 
@@ -50,13 +76,24 @@ end
 -- trabbit  
 --]]
 function trabbit:update()
+	if self.currInput ~= nil then 
+		self.prevInput = self.currInput 
+	end
+	for k in pairs(self.currInput) do 
+		self.currInput[k] = nil 
+	end
+
+	--get previous position before making any changes to the position 
+	self.prevX, self.prevY = self:getPosition() 
 	--if down is pressed, then move trabbit down 
 	if love.keyboard.isDown("cpaddown") or love.keyboard.isDown("down") then
 		self.y = self.y + 1.5	
+		self.currInput["down"] = true 
 	end
 	--if up is pressed, then move trabbit up 
 	if love.keyboard.isDown("cpadup") or love.keyboard.isDown("up") then 
 		self.y = self.y - 1.5
+		self.currInput["up"] = true 
 	end
 	--if right is pressed, then move trabbit right 
 	if love.keyboard.isDown("cpadright") or love.keyboard.isDown("right") then 
@@ -65,6 +102,7 @@ function trabbit:update()
 			self.x = self.x - self.width 
 		end
 		self.isFlipped = false
+		self.currInput["right"] = true 
 	end
 	--if left is pressed, then move trabbit left 
 	if love.keyboard.isDown("cpadleft") or love.keyboard.isDown("left") then 
@@ -73,7 +111,9 @@ function trabbit:update()
 			self.x = self.x + self.width
 		end
 		self.isFlipped = true 
+		self.currInput["left"] = true
 	end
+
 	--if time is not null, set previous time to time 
 	if time ~= nil then 
 		self.prevTime = time
@@ -81,19 +121,43 @@ function trabbit:update()
 	--get current time in seconds 
 	time = math.floor(love.timer.getTime())
 	if time - self.prevTime == 0 then 
+			self.activeFrame = 4
 		return 
 	end
 	--update current frame every second 
 	self.activeFrame = (self.activeFrame + 1) % 4
-	--logic for the different breathing frames 
+	--logic for the different breathing frames
 	if self.activeFrame == 0 then 
 		self.activeFrame = 4
-		self.y = self.y + 1
-	elseif self.activeFrame == 2 then 
-		self.y = self.y - 1
-	elseif self.activeFrame == 3 then 
-		self.y = self.y + 1
-	elseif self.activeFrame == 1 then 
-		self.y = self.y - 1
+	end
+
+	if self.currState == self.stateEnum["breathing"] then 
+		if self.activeFrame == 4 then 
+			self.y = self.y + 1
+		elseif self.activeFrame == 2 then 
+			self.y = self.y - 1
+		elseif self.activeFrame == 3 then 
+			self.y = self.y + 1
+		elseif self.activeFrame == 1 then 
+			self.y = self.y - 1
+		end
+	end
+
+	local switched = false 
+	if #self.prevInput == 0 and self.currInput["right"] ~= nil then 
+		switched = true 
+	elseif #self.prevInput == 0 and self.currInput["left"] ~= nil then 
+		switched = true
+	end
+	if switched then 
+		if self.currState ~= self.stateEnum["walking"] then 
+			self.currState = self.stateEnum["walking"]		
+			self.activeFrame = 1 
+		end  
+	else 
+		if self.currState ~= self.stateEnum["breathing"] then 
+			self.currState = self.stateEnum["breathing"] 
+			self.activeFrame = 1 
+		end
 	end
 end
